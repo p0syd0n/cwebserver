3e361c9c72e0a8d4c2ceee18fc736a5ae63ad9aa
p0syd0n <ajeff9752@gmail.com>
2025-08-19 03:30:56 +0000

    renamed buckets to nodes

--- src/hashtable.c
+++ src/hashtable.c
@@ -17,21 +17,22 @@ unsigned long hash_djb2(const char *str) {
     return hash;
 }
 
-typedef struct Bucket {
-  char* key;
-  char* value;
-  struct Bucket* next;
-  struct Bucket* previous;
-} Bucket;
+typedef struct Node {
+  char* key; // Pointer to key string
+  char* value; // Pointer to value string
+  struct Node* next; // Pointer to next item in linked list
+  struct Node* previous; // Pointer to previous item in linked list
+} Node;
+
 typedef struct Hashtable {
-  size_t size;
-  Bucket** table;
+  size_t size; // Size, so we don't have to rely on a macro
+  Node** table; // Pointer to Node pointer array
 } Hashtable;
 
 Hashtable* create_hashtable(size_t size) {
   Hashtable* hashtable = (Hashtable*)malloc(sizeof(Hashtable));
 
-  Bucket** thetable = calloc(size, sizeof(Bucket*));
+  Node** thetable = calloc(size, sizeof(Node*)); // Set all the nodes to NULL pointers at the beginning, to show that they are empty
   hashtable->table = thetable;
   hashtable->size = size;
 
@@ -40,36 +41,40 @@ Hashtable* create_hashtable(size_t size) {
 
 int table_add(Hashtable* hashtable, char* key, char* value) {
   unsigned long hash = hash_djb2(key);
-  int dictionary_index = hash % (hashtable->size);
+  int dictionary_index = hash % (hashtable->size); // Will always return a value between 0 and size-1 inclusive
 
-  Bucket* tempbucket = (hashtable->table)[dictionary_index];
+  Node* tempnode = (hashtable->table)[dictionary_index]; // This is the pointer to the head of the linked list for the given dictionary index
   
-  if (tempbucket == NULL) {
-    (hashtable->table)[dictionary_index] = (Bucket*)malloc(sizeof(Bucket));
-    tempbucket = (hashtable->table)[dictionary_index];
-    tempbucket->key = strdup(key);
-    tempbucket->value = strdup(value);
-    tempbucket->next = NULL;
-    tempbucket->previous = NULL;
+  if (tempnode == NULL) { // It's empty (there is no head. There is no leading node.). We make a new one.
+    (hashtable->table)[dictionary_index] = (Node*)malloc(sizeof(Node));
+    tempnode = (hashtable->table)[dictionary_index];
+    tempnode->key = strdup(key); // We take a copy in case the user decides to free his instance of the variable
+    tempnode->value = strdup(value); // Same thing here
+    tempnode->next = NULL; // There is no next one yet
+    tempnode->previous = NULL; // We are the head so there is no previous node
     return 0;
   }
 
-  Bucket* last;
-  while (tempbucket != NULL) {
-    if (strcmp(tempbucket->key, key) == 0) {
-      free(tempbucket->value);
-      tempbucket->value = strdup(value);
+  // Slightly convoluted logic to loop through the linked list of nodes with the given dictionary index
+  while (tempnode != NULL) {
+    // This is the update case. We found the value, it is already in the table.
+    if (strcmp(tempnode->key, key) == 0) {
+      free(tempnode->value); // We own this - we strduped it earlier.
+      tempnode->value = strdup(value); // The new value is added
       return 0;
     }
-    last = tempbucket;
-    tempbucket = (Bucket *)(tempbucket->next);
+    if (tempnode->next == NULL) break; // If we are on the last node, break out. We have not found a node with our key.
+
+    // The previous null check case was not true, so there is another node after this one. 
+    tempnode = (Node *)(tempnode->next);
   }
 
-  Bucket* next_prep = (Bucket*)malloc(sizeof(Bucket));
-  last->next = next_prep;
-  next_prep->key = strdup(key);
-  next_prep->value = strdup(value);
-  next_prep->previous = last;
+  // We did not find a node with the key provided. We must add one at the end of the linked list for this dictionary index.
+  Node* next_prep = (Node*)malloc(sizeof(Node));
+  tempnode->next = next_prep;
+  next_prep->key = strdup(key); // Again, strduping to make sure we have ownership of the values in the dictionary.
+  next_prep->value = strdup(value); // Otherwise, they may get free'd if the caller decides to free them. 
+  next_prep->previous = tempnode;
   next_prep->next = NULL;
   return 0;
 }
@@ -77,39 +82,49 @@ int table_add(Hashtable* hashtable, char* key, char* value) {
 int table_remove(Hashtable* hashtable, char* key) {
   unsigned long hash = hash_djb2(key);
   int dictionary_index = hash % (hashtable->size);
-  Bucket* tempbucket = (hashtable->table)[dictionary_index];
+  Node* tempnode = (hashtable->table)[dictionary_index];
 
-  if (tempbucket == NULL) { errno = ENOENT; return -1; }
+  if (tempnode == NULL) { errno = ENOENT; return -1; } // It does not exist. Thus, we create problems for the user to handle. Mwahahaha
 
-  while (strcmp(tempbucket->key, key) != 0) {
-    if (tempbucket->next == NULL) {
+  while (strcmp(tempnode->key, key) != 0) {
+    if (tempnode->next == NULL) {
+      // The last node is not it and we have not found the desired key.
       errno = ENOENT;
       return -1;
     }
-    tempbucket = (Bucket *)(tempbucket->next);
+    // We have not yet reached the end. We continue down the linked list.
+    tempnode = (Node *)(tempnode->next);
   }
 
-  if (tempbucket->previous == NULL && tempbucket->next == NULL) {
+
+  // Are all of these really necessary?
+
+  if (tempnode->previous == NULL && tempnode->next == NULL) {
+    // There is not a node before me and there is not a node after me. I am the only node with my dictionary index.
     (hashtable->table)[dictionary_index] = NULL;
 
-  } else if (tempbucket->next != NULL && tempbucket->previous != NULL) {
-    (tempbucket->next)->previous = tempbucket->previous;
-    (tempbucket->previous)->next = tempbucket->next;
+  } else if (tempnode->next != NULL && tempnode->previous != NULL) {
+    // There is a node after me and a node before me.
+    (tempnode->next)->previous = tempnode->previous; // Hey, node after me! (The node before you) (previously me) is now the node before me!
+    (tempnode->previous)->next = tempnode->next; // Hey, node before me! (The node after you) (previously me) is now the node after me!
 
-  } else if (tempbucket->next != NULL && tempbucket->previous == NULL) {
-    (hashtable->table)[dictionary_index] = tempbucket->next;
-    (tempbucket->next)->previous = NULL;
+  } else if (tempnode->next != NULL && tempnode->previous == NULL) {
+    // There is a node after me and not a node before me.
+    (hashtable->table)[dictionary_index] = tempnode->next; // Set the dictionary index pointer to point at the node after me. It is the new head.
+    (tempnode->next)->previous = NULL; // It does not have a node before it now. It is the head.
 
-  }  else if (tempbucket->next == NULL && tempbucket->previous != NULL) {
-    (tempbucket->previous)->next = NULL;
+  }  else if (tempnode->next == NULL && tempnode->previous != NULL) {
+    // There is not a node after me and there is a node before me.
+    (tempnode->previous)->next = NULL; // Hey, node before me! I no longer exist.
 
   } else {
     return -1; // tf?
   }
 
-  free(tempbucket->key);
-  free(tempbucket->value);
-  free(tempbucket);
+  // Ritual suicide.
+  free(tempnode->key);
+  free(tempnode->value);
+  free(tempnode);
 
 
 
@@ -120,29 +135,33 @@ char* table_get(Hashtable* hashtable, char* key) {
   unsigned long hash = hash_djb2(key);
   int dictionary_index = hash % (hashtable->size);
 
-  Bucket* tempbucket = (hashtable->table)[dictionary_index];
+  Node* tempnode = (hashtable->table)[dictionary_index];
 
-  while (tempbucket != NULL) {
-    if (strcmp(tempbucket->key, key) == 0) {
-      return tempbucket->value;
+  while (tempnode != NULL) {
+    // If its not NULL, check if the key matches.
+    if (strcmp(tempnode->key, key) == 0) {
+      // strdup here would be safer. But it also almost doubles time taken. The caller needs to be careful not to free the pointer they are given.
+      return tempnode->value;
     }
-    tempbucket = tempbucket->next;
+    tempnode = tempnode->next;
   }
 
+  // If we got to this point, we did not find a node, and broke out of the loop because the new node in the linked list was NULL.
   errno = ENOENT;
   return NULL;
 }
 
 
 void free_hashtable(Hashtable* hashtable) {
-    for (size_t i = 0; i < hashtable->size; i++) {
-        Bucket* bucket = hashtable->table[i];
-        while (bucket) {
-            Bucket* next = bucket->next;
-            free(bucket->key);
-            free(bucket->value);
-            free(bucket);
-            bucket = next;
+    for (size_t node_pointer_index = 0; node_pointer_index < hashtable->size; node_pointer_index++) {
+        Node* node = (hashtable->table)[node_pointer_index];
+        // Just go down the linked list freeing everybody
+        while (node) {
+            Node* next = node->next;
+            free(node->key);
+            free(node->value);
+            free(node);
+            node = next;
         }
     }
     free(hashtable->table);
